9e7722e38c7bf74a87e377570a198c9ae50638ce
Michael Silva <michaelcrf@gmail.com>
2026-04-09 13:02:03 -0300

    Adiciona classe Usuario

---
+++ src/Usuario.java
@@ -0,0 +1,10 @@
+package Entities;
+
+public class Usuario {
+
+    public int i;
+    public String[] nome = new String[10];
+    public int[] id = new int[10];
+    public String[] tipo = new String[10];
+
+}
